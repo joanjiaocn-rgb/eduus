4b9fc1a32cf3c326dea13fafa525c0027d964fcf
joanjiaocn-rgb <joanjiaocn@gmail.com>
2026-04-19 18:40:25 +0800

    fix: add type annotation to Functions context parameter

--- functions/api/hello.js
+++ functions/api/hello.js
@@ -1,4 +1,4 @@
-export async function onRequest(context) {
+export async function onRequest(context: any) {
   return new Response(JSON.stringify({ status: 'ok', message: 'Native Function works' }), {
     status: 200,
     headers: { 'Content-Type': 'application/json' }
